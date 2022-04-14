a444a7c31b66ac1b192ede4eccdbceea88e5e291
kehindeBankole <bankolek1@gmail.com>
2022-04-14 01:08:08 +0100

    code clean up

--- src/components/our-services/OurServices.tsx
+++ src/components/our-services/OurServices.tsx
@@ -3,18 +3,21 @@ import { Talent, Booking, BrandPartner, LabelService, Management, PR } from '../
 function OurServices() {
 const services=[{
 	title:"talent management"
-},
+,img:Talent},
 {
 	title:"bookings"
-},
+,img:Booking},
 {
 	title:"pr"
-},
+,img:PR},
 {
 	title:<>Social media <br /> management</>
+,img:Management},
+{
+	title:"label services",img:LabelService
 },
 {
-	title:"label services"
+	title:"brand patnership",img:BrandPartner
 }
 ]
 
@@ -25,7 +28,7 @@ const services=[{
 				<div className={`flex flex-wrap justify-center ${styles.service}`}>
 					{services.map((item,index)=>(
 						<div key={index} className="text-center uppercase md:text-[1.5rem]">
-						<img src={Talent} alt="service image" />
+						<img src={item.img} alt="service image" />
 						<p className="lg:mb-[72px] mb-[20px]">{item.title}</p>
 					</div>
 					))}
