afe3266e186031a802d6fbee8a001fe42e57216e
kehindeBankole <bankolek1@gmail.com>
2022-04-25 01:26:20 +0100

    removed gsap animation and integrated email.js on book artist section

--- src/utilities/TextArea/TextArea.tsx
+++ src/utilities/TextArea/TextArea.tsx
@@ -4,12 +4,13 @@ interface Props {
   height?: string;
   width?: string;
   onChange?:any;
-  value?:string
+  value?:string;
+  important?:any;
 }
 function TextArea(props: Props) {
   return (
     <div>
-      <label htmlFor="message">{props.label}</label>
+      <label htmlFor="message">{props.label}{props.important&&"*"}</label>
       <textarea name="message" onChange={(e)=>props.onChange(e.target.value)} value={props.value} className={`${styles.textarea} `}></textarea>
     </div>
   );
